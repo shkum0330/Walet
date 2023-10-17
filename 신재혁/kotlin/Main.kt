ed2eb346170cfe36e4f3777289a2e1470a3ea2a8
김보석 <st02045@naver.com>
2023-10-17 09:57:20 +0900

    Docs : template upload / unnecessary files delete

--- 신재혁/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    val pi : Double = 3.14
-    println(pi)
-
-    var age : Int = 28;
-    println(age)
-    age = 3
-    println(age)
-}
--- 유지나/testApp/app/src/main/java/com/example/testapp/Member.kt
+++
@@ -1,9 +0,0 @@
-package com.example.testapp
-
-data class Member(
-
-    val memberId:Long?=null,
-    val name:String?=null,
-    val email:String?=null
-
-)
