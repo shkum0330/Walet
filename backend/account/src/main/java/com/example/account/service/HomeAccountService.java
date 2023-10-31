6d5d07c38c79c4a892db85367d57e5970dfc4e83
codelee <leesy399@naver.com>
2023-10-31 16:51:29 +0900

    [BE] Docs : 계좌 Service 설명 추가

--- backend/account/src/main/java/com/example/account/service/HomeAccountService.java
+++ backend/account/src/main/java/com/example/account/service/HomeAccountService.java
@@ -3,5 +3,5 @@ package com.example.account.service;
 import com.example.account.api.response.account.HomeAccountResponse;
 
 public interface HomeAccountService {
-    HomeAccountResponse getHomeAccountDetail(Long accountId);
+    HomeAccountResponse getHomeAccountDetail(Long accountId); // 계좌 메인페이지에서 계좌 기본정보와 최근 거래목록 5개를 보여줌
 }
--- backend/account/src/main/java/com/example/account/service/TransactionService.java
+++ backend/account/src/main/java/com/example/account/service/TransactionService.java
@@ -9,11 +9,11 @@ import com.example.account.api.response.transaction.TransactionResponse;
 import java.util.List;
 
 public interface TransactionService {
-    TransactionAccountResponse getHomeAccountDetail(Long accountId);
+    TransactionAccountResponse getHomeAccountDetail(Long accountId); // 거래내역 페이지에서 계좌의 기본정보 반환
 
-    Long addPetRelatedTransaction(TransactionRequest transactionRequest);
+    Long addPetRelatedTransaction(TransactionRequest transactionRequest); // 반려동물 관련 상품 구매 거래내역 추가
 
-    Long addRemittanceTransaction(RemittanceRequest remittanceRequest);
-    List<TransactionResponse> getTransactionHistory(Long accountId);
-    List<TransactionResponse> getSpecificPeriodTransaction(TransactionPeriodRequest request);
+    Long addRemittanceTransaction(RemittanceRequest remittanceRequest); // 일반계좌 간 송금 거래내역 추가
+    List<TransactionResponse> getTransactionHistory(Long accountId); // 특정 계좌의 거래내역 목록 반환
+    List<TransactionResponse> getSpecificPeriodTransaction(TransactionPeriodRequest request); // 특정 기간 내에 속하는 거래내역 목록 반환
 }
