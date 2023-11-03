5b37937d17f24f47309e307eeb06b4b6dada0b42
송희도 <roeyr@naver.com>
2023-11-03 12:36:05 +0900

    [BE] Fix: oauth 테스트

--- backend/auth/src/main/java/com/ssafy/external/client/NHClient.java
+++ backend/auth/src/main/java/com/ssafy/external/client/NHClient.java
@@ -4,8 +4,6 @@ import com.ssafy.external.dto.NHDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 //@FeignClient(url = "https://nhopenapi.nonghyup.com/svcapi" , name = "NHClient")
