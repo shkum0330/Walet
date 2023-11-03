a71f0a28b1e5401c9d34459e05a045da160f0b10
송희도 <roeyr@naver.com>
2023-11-03 12:40:07 +0900

    [BE] Fix: oauth 테스트

--- backend/auth/src/main/java/com/ssafy/external/client/NHClient.java
+++ backend/auth/src/main/java/com/ssafy/external/client/NHClient.java
@@ -2,18 +2,18 @@ package com.ssafy.external.client;
 
 import com.ssafy.external.dto.NHDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 //@FeignClient(url = "https://nhopenapi.nonghyup.com/svcapi" , name = "NHClient")
 @FeignClient(url = "http://121.162.108.65:9460/svcapi" , name = "NHClient")
 public interface NHClient {
     @PostMapping(value = "/oauth/requestToken.nhd" , consumes = "application/x-www-form-urlencoded; charset=UTF-8")
-    NHDto.Response getKey(@PathVariable("client_id") String clientId,
-                         @PathVariable("client_secret") String clientSecret,
-                         @PathVariable("scope") String scope,
-                         @PathVariable("grant_type") String grantType);
+    NHDto.Response getKey(@RequestParam("client_id") String clientId,
+                         @RequestParam("client_secret") String clientSecret,
+                         @RequestParam("scope") String scope,
+                         @RequestParam("grant_type") String grantType);
 //    @PostMapping(value = "/oauth/requestToken.nhd" , consumes = "application/x-www-form-urlencoded; charset=UTF-8")
 //    NHDto.Response getKey(@RequestBody NHDto.Request request);
 }
