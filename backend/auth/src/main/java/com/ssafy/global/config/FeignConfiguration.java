96ce8eefe7825e0922ce751eec0dbf568b5c6933
송희도 <roeyr@naver.com>
2023-11-03 13:11:48 +0900

    [BE] Fix: oauth 테스트

--- backend/auth/src/main/java/com/ssafy/global/config/FeignConfiguration.java
+++ backend/auth/src/main/java/com/ssafy/global/config/FeignConfiguration.java
@@ -3,9 +3,15 @@ package com.ssafy.global.config;
 import com.ssafy.global.FeignClientExceptionErrorDecoder;
 import feign.Logger;
 import feign.Retryer;
+import feign.codec.Encoder;
 import feign.codec.ErrorDecoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,6 +21,8 @@ import org.springframework.context.annotation.Import;
 @EnableFeignClients(basePackages = "com.ssafy")
 @Import(FeignClientsConfiguration.class)
 public class FeignConfiguration {
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
     @Bean
     Logger.Level feignLoggerLevel(){
         return Logger.Level.FULL;
@@ -28,5 +36,10 @@ public class FeignConfiguration {
     @Bean
     public Retryer retryer(){
         return new Retryer.Default(1000,2000,3);
+    }
+
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
 }
