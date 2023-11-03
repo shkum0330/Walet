327b7e823e87502764f6ebe2befe6987e954b274
송희도 <roeyr@naver.com>
2023-11-03 15:13:10 +0900

    [BE] Fix: oauth 테스트

--- backend/auth/src/main/java/com/ssafy/external/client/NHClientDev.java
+++ backend/auth/src/main/java/com/ssafy/external/client/NHClientDev.java
@@ -1,15 +1,14 @@
 package com.ssafy.external.client;
 
 import com.ssafy.external.dto.NHDto;
-import com.ssafy.global.config.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url = "http://smartdev.nonghyup.com:9460/svcapi" , name = "NHClientDev" , configuration = FeignConfiguration.class)
+@FeignClient(url = "http://smartdev.nonghyup.com:9460/svcapi" , name = "NHClientDev")
 public interface NHClientDev {
     @PostMapping(value = "/oauth/requestToken.nhd" , consumes = "application/x-www-form-urlencoded; charset=UTF-8")
-    NHDto.Response getKey(@RequestParam("client_id") String clientId,
+    NHDto.Response getKeyDev(@RequestParam("client_id") String clientId,
                           @RequestParam("client_secret") String clientSecret,
                           @RequestParam("scope") String scope,
                           @RequestParam("grant_type") String grantType);
