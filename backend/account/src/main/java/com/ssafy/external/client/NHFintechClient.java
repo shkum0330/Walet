fb0dcb338d8196495d4f907e8ba8711bfb267a75
roeyr <roeyr@naver.com>
2023-11-04 22:45:44 +0900

    [BE] Feat: NH 오픈플랫폼 사용 메소드 추가

--- backend/account/src/main/java/com/ssafy/external/client/NHFintechClient.java
+++ backend/account/src/main/java/com/ssafy/external/client/NHFintechClient.java
@@ -3,11 +3,48 @@ package com.ssafy.external.client;
 import com.ssafy.account.config.ClientProxyConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(url = "http://smartdev.nonghyup.com:9460/svcapi" , name = "Server1" , configuration = ClientProxyConfig.class)
 public interface NHFintechClient {
-    @PostMapping(value = "/OpenFinAccountARS.nhd")
-    String OpenFinAccountARS(@RequestHeader("ApiNm") String name);
+    @PostMapping(value = "/OpenFinAccountARS.nhd" , headers = "ApiNm=OpenFinAccountARS")
+    String OpenFinAccountARS();
 
+    @PostMapping(value = "/CheckOpenFinAccount.nhd" , headers = "ApiNm=CheckOpenFinAccount")
+    String CheckOpenFinAccount();
+
+    @PostMapping(value = "/InquireFinAccountStatus.nhd" , headers = "ApiNm=InquireFinAccountStatus")
+    String InquireFinAccountStatus();
+
+    @PostMapping(value = "/CloseFinAccount.nhd" , headers = "ApiNm=CloseFinAccount")
+    String CloseFinAccount();
+
+    @PostMapping(value = "/P2PNVirtualAccountNumberRequest.nhd" , headers = "ApiNm=P2PNVirtualAccountNumberRequest")
+    String P2PNVirtualAccountNumberRequest();
+
+    @PostMapping(value = "/P2PNVirtualAccountTerminate.nhd" , headers = "ApiNm=P2PNVirtualAccountTerminate")
+    String P2PNVirtualAccountTerminate();
+
+    @PostMapping(value = "/InquireTransactionHistory.nhd" , headers = "ApiNm=InquireTransactionHistory")
+    String InquireTransactionHistory();
+
+    @PostMapping(value = "/InquireBalance.nhd" , headers = "ApiNm=InquireBalance")
+    String InquireBalance();
+
+    @PostMapping(value = "/InquireDepositorFinAccount.nhd" , headers = "ApiNm=InquireDepositorFinAccount")
+    String InquireDepositorFinAccount();
+
+    @PostMapping(value = "/CheckDepositorFinAccount.nhd" , headers = "ApiNm=CheckDepositorFinAccount")
+    String CheckDepositorFinAccount();
+
+    @PostMapping(value = "/DrawingTransfer2.nhd" , headers = "ApiNm=DrawingTransfer2")
+    String DrawingTransfer2();
+
+    @PostMapping(value = "/CheckOnDrawingTransfer2.nhd" , headers = "ApiNm=CheckOnDrawingTransfer2")
+    String CheckOnDrawingTransfer2();
+
+    @PostMapping(value = "/ReceivedTransferFinAccount.nhd" , headers = "ApiNm=ReceivedTransferFinAccount")
+    String ReceivedTransferFinAccount();
+
+    @PostMapping(value = "/CheckOnReceivedTransfer.nhd" , headers = "ApiNm=CheckOnReceivedTransfer")
+    String CheckOnReceivedTransfer();
 }
