6184e1884e5e6b464c3bd6d58344143378bf354a
송희도 <roeyr@naver.com>
2023-11-07 14:04:50 +0900

    [BE] Fix: 핀어카운트 발급 Dto 수정

--- backend/service/src/main/java/com/ssafy/service/external/dto/CheckOpenFinAccountDto.java
+++ backend/service/src/main/java/com/ssafy/service/external/dto/CheckOpenFinAccountDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.service.external.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,16 +12,26 @@ public class CheckOpenFinAccountDto {
     @Builder
     @AllArgsConstructor
     public static class Request{
+        @JsonProperty("Header")
+        HeaderDto Header;
+        @JsonProperty("Rgno")
         String Rgno;
+        @JsonProperty("BrdtBrno")
         String BrdtBrno;
+        @JsonProperty("Tlno")
         String Tlno;
     }
 
     @Getter
     @Setter
     public static class Response{
+        @JsonProperty("Header")
+        HeaderDto Header;
+        @JsonProperty("FinAcno")
         String FinAcno;
+        @JsonProperty("RgsnYmd")
         String RgsnYmd;
+        @JsonProperty("Acno")
         String Acno;
     }
 }
