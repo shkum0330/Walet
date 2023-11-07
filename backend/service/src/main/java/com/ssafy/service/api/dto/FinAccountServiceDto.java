e793297e24c08e075d2611169eabd9075dc2fc3b
송희도 <roeyr@naver.com>
2023-11-07 13:00:49 +0900

    [BE] Feat: Dto 속성명 추가

--- backend/service/src/main/java/com/ssafy/service/api/dto/FinAccountServiceDto.java
+++ backend/service/src/main/java/com/ssafy/service/api/dto/FinAccountServiceDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.service.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +8,15 @@ public class FinAccountServiceDto {
     @Getter
     @Setter
     public static class Request{
+        @JsonProperty("Csnm")
         String Csnm;
+        @JsonProperty("BrdtBrno")
         String BrdtBrno;
+        @JsonProperty("Tlno")
         String Tlno;
+        @JsonProperty("Acno")
         String Acno;
+        @JsonProperty("DrtrRgyn")
         String DrtrRgyn;
     }
 }
