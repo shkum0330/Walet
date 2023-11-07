6646d555345ccafcafc7c1d1456d89d02e9c1608
송희도 <roeyr@naver.com>
2023-11-07 14:25:27 +0900

    [BE] Fix: 핀어카운트 발급 수정

--- backend/service/src/main/java/com/ssafy/service/external/service/NHFintechServiceImpl.java
+++ backend/service/src/main/java/com/ssafy/service/external/service/NHFintechServiceImpl.java
@@ -42,12 +42,16 @@ public class NHFintechServiceImpl implements NHFintechService{
             OpenFinAccountARSDto.Response OpenFinAccountARSResponse = objectMapper.readValue(OpenFinAccountARSJsonString , OpenFinAccountARSDto.Response.class);
             String CheckOpenFinAccountJsonString = CheckOpenFinAccount(OpenFinAccountARSResponse.getRgno() , data.getBrdtBrno() , data.getTlno() , key);
             CheckOpenFinAccountDto.Response CheckOpenFinAccountResponse = objectMapper.readValue(CheckOpenFinAccountJsonString , CheckOpenFinAccountDto.Response.class);
+            System.out.println(CheckOpenFinAccountResponse.toString());
+
 
             while(CheckOpenFinAccountResponse.getRpcd() != null && CheckOpenFinAccountResponse.getRpcd().equals("A0017")){
                 CheckOpenFinAccountJsonString = CheckOpenFinAccount(OpenFinAccountARSResponse.getRgno() , data.getBrdtBrno() , data.getTlno() , key);
                 CheckOpenFinAccountResponse = objectMapper.readValue(CheckOpenFinAccountJsonString , CheckOpenFinAccountDto.Response.class);
                 System.out.println(CheckOpenFinAccountResponse.toString());
             }
+
+
             FinAccountServiceDto.Response response = FinAccountServiceDto.Response.builder()
                     .FinAcno(CheckOpenFinAccountResponse.getFinAcno())
                     .build();
