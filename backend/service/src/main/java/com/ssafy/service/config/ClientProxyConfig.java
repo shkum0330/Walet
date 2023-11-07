f91dcc4acd8e8459060b90a27e2fec76a675fa04
송희도 <roeyr@naver.com>
2023-11-07 13:10:47 +0900

    [BE] Feat: Response Decodeder 추가

--- backend/service/src/main/java/com/ssafy/service/config/ClientProxyConfig.java
+++ backend/service/src/main/java/com/ssafy/service/config/ClientProxyConfig.java
@@ -3,19 +3,26 @@ package com.ssafy.service.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Client;
 import feign.Feign;
+import feign.codec.Decoder;
 import feign.codec.Encoder;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class ClientProxyConfig {
+    private ObjectFactory<HttpMessageConverters> messageConverters;
     @Bean
-    public Feign.Builder feignBuilder(Client feignClientProxy , Encoder encoder) {
+    public Feign.Builder feignBuilder(Client feignClientProxy , Encoder encoder , Decoder decoder) {
         return Feign.builder()
                 .encoder(encoder)
+                .decoder(decoder)
                 .client(feignClientProxy);
     }
 
@@ -23,5 +30,11 @@ public class ClientProxyConfig {
     @Primary
     public Encoder feignEncoder() {
         return new ByteArrayEncoder(new ObjectMapper());
+    }
+
+    @Bean
+    @Primary
+    public Decoder feignDecoder() {
+        return new CustomDecoder(messageConverters);
     }
 }
