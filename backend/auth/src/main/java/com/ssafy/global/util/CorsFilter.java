961b10e7bbd16c8e88c13d77bbfe11cfbd8ad9a2
김보석 <st02045@naver.com>
2023-11-08 14:33:55 +0900

    [BE] Fix : CORS 에러처리

--- backend/auth/src/main/java/com/ssafy/global/util/CorsFilter.java
+++ backend/auth/src/main/java/com/ssafy/global/util/CorsFilter.java
@@ -1,6 +1,6 @@
 package com.ssafy.global.util;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -9,14 +9,13 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    @Value("#{'${cors.origins}'.split(',')}")
-    private List<String> origins;
+
 
 
     @Override
@@ -24,11 +23,8 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        String origin = request.getHeader("Origin");
-        if (origin != null && origins.contains(origin)) {
-            response.setHeader("Access-Control-Allow-Origin", origin);
-        }
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "false");
         response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
