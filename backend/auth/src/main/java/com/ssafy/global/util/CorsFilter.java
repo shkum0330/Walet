04f2d72a217619440b8082e725ed53e07bc5079a
김보석 <st02045@naver.com>
2023-11-08 14:39:02 +0900

    [BE] Fix : CORS 에러처리

--- backend/auth/src/main/java/com/ssafy/global/util/CorsFilter.java
+++ backend/auth/src/main/java/com/ssafy/global/util/CorsFilter.java
@@ -15,9 +15,6 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-
-
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
@@ -26,9 +23,8 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "false");
         response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Max-Age", "*");
+        response.setHeader("Access-Control-Allow-Headers","*");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
