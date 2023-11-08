f0e13bacab1d1f00ce5960cf507e6e9fb4354b10
김보석 <st02045@naver.com>
2023-11-08 13:45:51 +0900

    [BE] Fix : 배포 서버 CORS 해결

--- backend/notice/src/main/java/com/ssafy/global/util/CorsFilter.java
+++ backend/notice/src/main/java/com/ssafy/global/util/CorsFilter.java
@@ -1,5 +1,6 @@
 package com.ssafy.global.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -8,24 +9,27 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    @Value("#{'${cors.origins}'.split(',')}")
+    private List<String> origins;
 
-    }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        String origin = request.getHeader("Origin");
+        if (origin != null && origins.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -35,10 +39,5 @@ public class CorsFilter implements Filter {
         }else {
             chain.doFilter(req, res);
         }
-    }
-
-    @Override
-    public void destroy() {
-
     }
 }
