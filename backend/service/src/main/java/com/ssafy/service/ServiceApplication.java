1d7d9972c7f21821b6727222aaead9dd5aae0f11
송희도 <roeyr@naver.com>
2023-11-09 11:19:40 +0900

    [BE] Fix: server timezone 수정

--- backend/service/src/main/java/com/ssafy/service/ServiceApplication.java
+++ backend/service/src/main/java/com/ssafy/service/ServiceApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ServiceApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
