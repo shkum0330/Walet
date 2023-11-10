5ba00baece2ab4ee1793d9939af75146f7353278
codelee <leesy399@naver.com>
2023-11-10 15:41:58 +0900

    [BE] Feat : 관리자페이지 계좌 관련 로직 변경(전체 회원의 계좌 목록 반환 -> 회원의 PK도 함께 반환)

--- backend/account/src/main/java/com/ssafy/account/api/response/account/AdminMemberAccountResponse.java
+++ backend/account/src/main/java/com/ssafy/account/api/response/account/AdminMemberAccountResponse.java
@@ -6,12 +6,14 @@ import lombok.Data;
 @Data
 public class AdminMemberAccountResponse {
     private Long accountId; // 계좌 PK
+    private Long memberId; // 계좌 주인의 PK
     private String accountName; // 계좌명
     private String accountNumber; // 계좌번호
     private Long balance; // 계좌 잔액
 
     public AdminMemberAccountResponse(Account account) {
         this.accountId = account.getId();
+        this.memberId = account.getMemberId();
         this.accountName = account.getAccountName();
         this.accountNumber = account.getAccountNumber();
         this.balance = account.getBalance();
