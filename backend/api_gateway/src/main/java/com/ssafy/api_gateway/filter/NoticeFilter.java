74baa7780b8f1ceab9e5513b47f0d7c08d7130b2
송희도 <roeyr@naver.com>
2023-11-15 10:15:03 +0900

    [BE] Fix: 테스트

--- backend/api_gateway/src/main/java/com/ssafy/api_gateway/filter/NoticeFilter.java
+++ backend/api_gateway/src/main/java/com/ssafy/api_gateway/filter/NoticeFilter.java
@@ -43,6 +43,8 @@ public class NoticeFilter extends AbstractGatewayFilterFactory<NoticeFilter.Conf
             System.out.println(request.getMethod().matches(HttpMethod.GET));
             System.out.println(request.getURI().getPath());
             System.out.println(userType);
+            System.out.println(userType.equals("\"ADMIN\""));
+            System.out.println(userType.equals("\"USER\""));
 //          공지사항 조회일경우 USER타입 허용
             if (request.getMethod() != null && request.getMethod().matches(HttpMethod.GET) && request.getURI().getPath().equals("/api/notice/pop")) {
                 if(userType.equals("\"USER\"")){
