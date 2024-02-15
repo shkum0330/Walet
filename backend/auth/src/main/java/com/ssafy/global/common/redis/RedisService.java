3c36f13d41bfcbef5337f458de5f0f35f6b16c16
shkum0330 <shkum0330@gmail.com>
2024-02-15 15:22:01 +0900

    Feat: application.yml 생성

--- backend/auth/src/main/java/com/ssafy/global/common/redis/RedisService.java
+++ backend/auth/src/main/java/com/ssafy/global/common/redis/RedisService.java
@@ -5,7 +5,6 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
-
 @Service
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
