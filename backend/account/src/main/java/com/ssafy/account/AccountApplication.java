d3154779c06f99cee9e45001efa4691447d95711
shkum0330 <shkum0330@gmail.com>
2024-03-03 20:39:07 +0900

    Fix: 라이브러리 오류 해결

--- backend/account/src/main/java/com/ssafy/account/AccountApplication.java
+++ backend/account/src/main/java/com/ssafy/account/AccountApplication.java
@@ -21,9 +21,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 public class AccountApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
-
 }
